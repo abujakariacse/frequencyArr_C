0362a12caca312e98dd14bb1e31e2b7d5ed4d775
Abu Jakaria <abujakaria316@gmail.com>
2024-10-31 20:59:31 +0600

    frequency array implement in int arr

--- frequencyArr.c
+++ frequencyArr.c
@@ -2,6 +2,41 @@
 
 int main()
 {
-    printf("Hello, World!\n");
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    int frequencyArr[100000] = {0};
+
+    for (int i = 0; i < n; i++)
+    {
+        int element = arr[i];
+
+        frequencyArr[element] = 1;
+    }
+
+    int findLength;
+    scanf("%d", &findLength);
+
+    for (int i = 0; i < findLength; i++)
+    {
+        int x;
+        scanf("%d", &x);
+
+        if (frequencyArr[x] == 1)
+        {
+            printf("Ache ");
+        }
+        else
+        {
+            printf("Nai ");
+        }
+    }
     return 0;
 }
